c5623dc67a59d2cfd3c08e1406adca71030376f2
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-02-08 18:54:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -154,6 +154,17 @@ client.on('message', async function(message){
   }
   
 });
+// late night wish
+let now4 = new Date();
+let milsecTill18 = new Date(now4.getFullYear(), now4.getMonth(), now4.getDate(), 21, 0, 0, 0) - now4;
+if (milsecTill18 < 0) {
+     milsecTill18 += 86400000;
+}
+setTimeout(function(){
+  var yourchannel = client.channels.cache.get('711468609846771716');
+  yourchannel.send('beta so jao raat bahut ho gayi hai ab to soo jao @here btw i am sleeping good night ');
+  yourchannel.send('https://tenor.com/view/right-now-mad-angry-guardians-of-the-galaxy-chris-pratt-gif-15301397');
+}, milsecTill18);
 
 const Prefix = '^';
 client.on('message', function(message) {
