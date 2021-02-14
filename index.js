8e7090a1d9d4af36c19e8116ebe3d091f9d2498d
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-02-14 23:29:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -166,6 +166,17 @@ setTimeout(function(){
   yourchannel.send('https://tenor.com/view/pretty-sleep-beautysleep-gif-4919464');
 }, milsecTill18);
 
+let now4 = new Date();
+let milsecTill18 = new Date(now4.getFullYear(), now4.getMonth(), now4.getDate(), 18, 00, 0, 0) - now4;
+if (milsecTill18 < 0) {
+     milsecTill18 += 86400000;
+}
+setTimeout(function(){
+  var yourchannel = client.channels.cache.get('711468609846771716');
+  yourchannel.send('u should study now u have your exams<@732809280918519879> ');
+  yourchannel.send('https://media1.tenor.com/images/1ef01c587350f01201d5428cc280e438/tenor.gif?itemid=13758139');
+}, milsecTill18);
+
 const Prefix = '^';
 client.on('message', function(message) {
   if (message.content.toLocaleLowerCase().startsWith(Prefix + 'move')) {
